830955fbb84abf564d84e78f50425c5def8c521b
Sniya Jose <sniyajose18@Gmail.com>
2025-03-20 20:03:45 +0530

    Initial commit: Netflix Clone project

---
+++ src/Constants/constants.jsx
@@ -0,0 +1,4 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = "f91b14b4bc4fe01d220c03dddc27caa1";
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
+export const originals = 'https://api.themoviedb.org/3/discover/tv?api_key={API_KEY_HERE}&with_networks=213';
